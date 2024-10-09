7d79a51731290e625275110b1e6b6424b2e9d284
Quang Bui M <usociety03@gmail.com>
2024-10-09 11:22:09 +0700

    Add user login route

--- api/server.go
+++ api/server.go
@@ -1,21 +1,40 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	db "github.com/yelaco/simple-bank/db/sqlc"
+	"github.com/yelaco/simple-bank/token"
+	"github.com/yelaco/simple-bank/util"
 )
 
 // Server serves HTTP requests for our banking service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{
-		store: store,
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("api.NewServer: cannot create token maker: %w", err)
 	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
 	router := gin.Default()
 
 	_ = server.bindValidators()
@@ -35,10 +54,10 @@ func NewServer(store db.Store) *Server {
 	userRoutes := router.Group("/users")
 	{
 		userRoutes.POST("/", server.createUser)
+		userRoutes.POST("/login", server.loginUser)
 	}
 
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
